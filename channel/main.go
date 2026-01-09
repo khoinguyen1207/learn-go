7a3b908e78248f7b0d11818cb742978cd5682d58
nkhoinguyen <khoinguyentpvl@gmail.com>
2026-01-09 11:11:33 +0700

    waitgroup & channel

--- channel/main.go
+++ channel/main.go
@@ -1,22 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func task(i int, wg *sync.WaitGroup, ch chan string) {
+	defer wg.Done()
+	fmt.Println("Task started")
+	time.Sleep(1 * time.Second)
+	ch <- "42"
+}
 
 func main() {
-	var ch = make(chan int)
-
-	go func() {
-		defer close(ch)
-		ch <- 1
-		ch <- 2
-		ch <- 3
-	}()
+	var wg sync.WaitGroup
+	var ch = make(chan string)
+	// go func() {
+	// 	defer close(ch)
+	// 	ch <- 1
+	// 	ch <- 2
+	// 	ch <- 3
+	// }()
 
 	// for i := 0; i < 3; i++ {
 	// 	fmt.Println(<-ch)
 	// }
 
+	// for v := range ch {
+	// 	fmt.Println(v)
+	// }
+
+	// =====================
+	start := time.Now()
+
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go task(i, &wg, ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
 	for v := range ch {
 		fmt.Println(v)
 	}
+
+	fmt.Printf("✅ Main function completed. Elapsed time: %s\n", time.Since(start))
 }
